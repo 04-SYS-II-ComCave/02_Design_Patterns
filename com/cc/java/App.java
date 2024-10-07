2c26eb184680e5bee6f94181e5798ee7c71eb914
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-10-07 10:51:54 +0000

    01_Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,11 @@ public class App {
     
     public static void main(String[] args) {
      
+        Singleton singleton1 = new Singleton();
+    output("obj1" + singleton1);
+
+    Singleton singleton2 = new Singleton();
+    output("obj1" + singleton2);
     }
 
     private static void output(String outputStr) {
