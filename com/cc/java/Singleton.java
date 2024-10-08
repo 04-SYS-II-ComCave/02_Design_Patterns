fc29c0ad6a8f0ad70efa30df725588fb116c9ec7
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-10-08 08:26:23 +0000

    02/Helper-klasse-Überladen

--- com/cc/java/Singleton.java
+++ com/cc/java/Singleton.java
@@ -3,6 +3,16 @@ package com.cc.java;
 
 public class Singleton {
 
+   // Objektbildung 
+   private static  Singleton instance; 
 
+    private Singleton(){
+
+    }
+
+    public static  Singleton getInstance() {
+        return instance;
+    }
+    
     
 }
