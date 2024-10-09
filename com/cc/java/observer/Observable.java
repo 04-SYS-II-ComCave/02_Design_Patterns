7c676c0528a4604bd56aa95da39389accb5511aa
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-10-09 08:34:26 +0000

    04/Subject

---
+++ com/cc/java/observer/Observable.java
@@ -0,0 +1,10 @@
+package com.cc.java.observer;
+
+public interface Observable {
+
+    public abstract void attach(Observer o);
+    public abstract void detatch(Observer o);
+    public abstract void notifyObservers();
+
+
+}
